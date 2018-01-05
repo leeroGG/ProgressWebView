4db0b8c90bb7e8e5b21cbc2eb4f5a803b25e586e
leero <925230519@qq.com>
2018-01-05 14:58:17 +0800

    添加控件

--- app/src/main/java/com/example/administrator/progresswebview/SecondActivity.java
+++ app/src/main/java/com/example/administrator/progresswebview/SecondActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.administrator.progresswebview.widget.ProgressWebView;
+
 public class SecondActivity extends AppCompatActivity {
+
+    protected ProgressWebView mWebView;
 
     public static Intent newInstance(Context context) {
         return new Intent(context, SecondActivity.class);
@@ -15,5 +19,9 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        mWebView = (ProgressWebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://www.baidu.com");
     }
 }
